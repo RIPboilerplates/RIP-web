854950fc1a99b2c7fc7e2a5a57e8a0fa2659b1c0
Avi Charlop <avicharlop@gmail.com>
2018-01-29 15:04:33 -0500

    update react-loadable

--- app/containers/NotFoundPage/index.js
+++ app/containers/NotFoundPage/index.js
@@ -1,25 +1,9 @@
 /**
- * NotFoundPage
- *
- * This is the page we show when the user visits a url that doesn't have a route
- *
- * NOTE: while this component should technically be a stateless functional
- * component (SFC), hot reloading does not currently support SFCs. If hot
- * reloading is not a necessity for you then you can refactor it and remove
- * the linting exception.
+ * Asynchronously loads the component for NotFoundPage
  */
+import Loadable from 'react-loadable'
 
-import React from 'react'
-import { FormattedMessage } from 'react-intl'
-
-import messages from './messages'
-
-export default class NotFound extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
-  render() {
-    return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
-    )
-  }
-}
+export default Loadable({
+  loader:  () => import('./index'),
+  loading: () => null,
+})
