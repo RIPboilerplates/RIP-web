5a2758e8580fc888d3c7d2d8e2b3c94225775c03
Avi Charlop <avicharlop@gmail.com>
2018-04-28 01:43:33 +0100

    sync reduxConstant generator

--- internals/generators/reduxConstant/index.js
+++ internals/generators/reduxConstant/index.js
@@ -8,8 +8,8 @@ module.exports = {
   prompts:     [{
     type:     'input',
     name:     'name',
-    message:  'What reduxer should the changes be added to?',
     default:  'Navigation',
+    message:  'What reduxer should the changes be added to?',
     validate: (value) => {
       if ((/.+/).test(value)) {
         return checks.reducerExists(value) ? 'A reduxer with that name does not already exists, use redux generator to add reduxer' : true
@@ -20,8 +20,8 @@ module.exports = {
   }, {
     type:     'input',
     name:     'actionName',
-    message:  'What should the constant be named?',
     default:  'LOCATION_CHANGE',
+    message:  'What should the constant be named?',
     validate: (value) => {
       if ((/.+/).test(value)) {
         return checks.constantExists(value) ? 'A constant with that name already exists' : true
@@ -29,76 +29,107 @@ module.exports = {
 
       return 'The constant is required'
     },
+  }, {
+    type:    'confirm',
+    name:    'isAsync',
+    default: true,
+    message: 'Will the action dispatch an async thunk?',
+  // }, {
+  //   type:     'input',
+  //   name:     'stateVariable',
+  //   message:  'What variable in state should the data be stored in',
+  //   default:  'value',
+  //   validate: (value) => {
+  //     if (value.split(':').length === 2) {
+  //       return true
+  //     }
+  //
+  //     return 'A value'
+  //   },
   }],
-  actions: [{
-    type:         'modify',
-    path:         '../../app/redux/{{properCase name}}/constants.js',
-    pattern:      /(\/\*\seslint-enable no-multi-spaces\s\*\/\n)(?!\/\*\seslint-enable no-multi-spaces\s\*\/)/g,
-    templateFile: './reduxConstant/constants.hbs',
-  }, {
-    type:         'modify',
-    path:         '../../app/redux/{{properCase name}}/index.js',
-    pattern:      /(}\sfrom\s'.\/constants'\n)(?!}\sfrom\s'.\/constants')/,
-    templateFile: './reduxConstant/import-constant.hbs',
-  }, {
-    type:         'modify',
-    path:         '../../app/redux/{{properCase name}}/index.js',
-    pattern:      /(}\sfrom\s'.\/thunks'\n)(?!}\sfrom\s'.\/thunks')/,
-    templateFile: './reduxConstant/import-thunk.hbs',
-  }, {
-    type:         'modify',
-    path:         '../../app/redux/{{properCase name}}/index.js',
-    pattern:      /([\s\S]*)/,
-    templateFile: './reduxConstant/action.hbs',
-  }, {
-    type:         'modify',
-    path:         '../../app/redux/{{properCase name}}/reducer.js',
-    pattern:      /([ \t]+INITIAL_STATE,\n)(?![ \t]+INITIAL_STATE,)/,
-    templateFile: './reduxConstant/import-constants.hbs',
-  }, {
-    type:         'modify',
-    path:         '../../app/redux/{{properCase name}}/reducer.js',
-    pattern:      /([ \t]+default:\n)(?![ \t]+default:)/,
-    templateFile: './reduxConstant/reducer.hbs',
-  }, {
-    type:         'modify',
-    path:         '../../app/redux/{{properCase name}}/thunks.js',
-    pattern:      /([\s\S]*)/,
-    templateFile: './reduxConstant/thunk.hbs',
-  }, {
-    type:         'modify',
-    path:         '../../app/redux/{{properCase name}}/tests/index.test.js',
-    pattern:      /(}\sfrom\s'..\/'\n)(?!}\sfrom\s'..\/')/,
-    templateFile: './reduxConstant/import-action.hbs',
-  }, {
-    type:         'modify',
-    path:         '../../app/redux/{{properCase name}}/tests/index.test.js',
-    pattern:      /(}\sfrom\s'..\/constants'\n)(?!}\sfrom\s'..\/constants')/,
-    templateFile: './reduxConstant/import-constant.hbs',
-  }, {
-    type:         'modify',
-    path:         '../../app/redux/{{properCase name}}/tests/index.test.js',
-    pattern:      /(^}\)$)/gm,
-    templateFile: './reduxConstant/test-action.hbs',
-  }, {
-    type:         'modify',
-    path:         '../../app/redux/{{properCase name}}/tests/reducer.test.js',
-    pattern:      /([ \t]+INITIAL_STATE,\n)(?![ \t]+INITIAL_STATE,)/,
-    templateFile: './reduxConstant/import-constants.hbs',
-  }, {
-    type:         'modify',
-    path:         '../../app/redux/{{properCase name}}/tests/reducer.test.js',
-    pattern:      /(^}\)$)/gm,
-    templateFile: './reduxConstant/test-reducer.hbs',
-  }, {
-    type:         'modify',
-    path:         '../../app/redux/{{properCase name}}/tests/thunks.test.js',
-    pattern:      /(}\sfrom\s'..\/thunks'\n)(?!}\sfrom\s'..\/thunks')/,
-    templateFile: './reduxConstant/import-thunk.hbs',
-  }, {
-    type:         'modify',
-    path:         '../../app/redux/{{properCase name}}/tests/thunks.test.js',
-    pattern:      /(^}\)$)/gm,
-    templateFile: './reduxConstant/test-thunk.hbs',
-  }],
+  actions: (data) => {
+    const actions = [{
+      type:         'modify',
+      path:         '../../app/redux/{{properCase name}}/constants.js',
+      pattern:      /(\/\*\seslint-enable no-multi-spaces\s\*\/\n)(?!\/\*\seslint-enable no-multi-spaces\s\*\/)/g,
+      templateFile: './reduxConstant/constants.hbs',
+    }, {
+      type:         'modify',
+      path:         '../../app/redux/{{properCase name}}/index.js',
+      pattern:      /(}\sfrom\s'.\/constants'\n)(?!}\sfrom\s'.\/constants')/,
+      templateFile: './reduxConstant/import-constant.hbs',
+    }, {
+      type:         'modify',
+      path:         '../../app/redux/{{properCase name}}/index.js',
+      pattern:      /([\s\S]*)/,
+      templateFile: './reduxConstant/action.hbs',
+    }, {
+      type:         'modify',
+      path:         '../../app/redux/{{properCase name}}/reducer.js',
+      pattern:      /([ \t]+INITIAL_STATE,\n)(?![ \t]+INITIAL_STATE,)/,
+      templateFile: './reduxConstant/import-constants.hbs',
+    }, {
+      type:         'modify',
+      path:         '../../app/redux/{{properCase name}}/reducer.js',
+      pattern:      /([ \t]+default:\n)(?![ \t]+default:)/,
+      templateFile: './reduxConstant/reducer.hbs',
+    }, {
+      type:         'modify',
+      path:         '../../app/redux/{{properCase name}}/tests/index.test.js',
+      pattern:      /(}\sfrom\s'..\/'\n)(?!}\sfrom\s'..\/')/,
+      templateFile: './reduxConstant/import-action.hbs',
+    }, {
+      type:         'modify',
+      path:         '../../app/redux/{{properCase name}}/tests/index.test.js',
+      pattern:      /(}\sfrom\s'..\/constants'\n)(?!}\sfrom\s'..\/constants')/,
+      templateFile: './reduxConstant/import-constant.hbs',
+    }, {
+      type:         'modify',
+      path:         '../../app/redux/{{properCase name}}/tests/index.test.js',
+      pattern:      /(^}\)$)/gm,
+      templateFile: './reduxConstant/test-action.hbs',
+    }, {
+      type:         'modify',
+      path:         '../../app/redux/{{properCase name}}/tests/reducer.test.js',
+      pattern:      /([ \t]+INITIAL_STATE,\n)(?![ \t]+INITIAL_STATE,)/,
+      templateFile: './reduxConstant/import-constants.hbs',
+    }, {
+      type:         'modify',
+      path:         '../../app/redux/{{properCase name}}/tests/reducer.test.js',
+      pattern:      /(^}\)$)/gm,
+      templateFile: './reduxConstant/test-reducer.hbs',
+    }]
+
+    if (data.isAsync) {
+      actions.push({
+        type:         'modify',
+        path:         '../../app/redux/{{properCase name}}/index.js',
+        pattern:      /(}\sfrom\s'.\/thunks'\n)(?!}\sfrom\s'.\/thunks')/,
+        templateFile: './reduxConstant/import-thunk.hbs',
+      })
+
+      actions.push({
+        type:         'modify',
+        path:         '../../app/redux/{{properCase name}}/thunks.js',
+        pattern:      /([\s\S]*)/,
+        templateFile: './reduxConstant/thunk.hbs',
+      })
+
+      actions.push({
+        type:         'modify',
+        path:         '../../app/redux/{{properCase name}}/tests/thunks.test.js',
+        pattern:      /(}\sfrom\s'..\/thunks'\n)(?!}\sfrom\s'..\/thunks')/,
+        templateFile: './reduxConstant/import-thunk.hbs',
+      })
+
+      actions.push({
+        type:         'modify',
+        path:         '../../app/redux/{{properCase name}}/tests/thunks.test.js',
+        pattern:      /(^}\)$)/gm,
+        templateFile: './reduxConstant/test-thunk.hbs',
+      })
+    }
+
+    return actions
+  },
 }
