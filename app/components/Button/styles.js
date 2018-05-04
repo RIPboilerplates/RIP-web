8337994e75d5db74819249cf5099f7838e8195fa
Avi Charlop <avicharlop@gmail.com>
2018-05-04 19:32:38 +0100

    added metrics

--- app/components/Button/styles.js
+++ app/components/Button/styles.js
@@ -1,8 +1,10 @@
 import styled from 'styled-components'
 import Colors from 'themes/colors'
+import Metrics from 'themes/metrics'
 
 export default {
   Container: styled.button`
     background-color: ${Colors.snow};
+    padding: ${Metrics.paddingBase};
   `,
 }
---
+++ app/themes/metrics.js
@@ -0,0 +1,10 @@
+const metrics = {
+  marginBase:   '15px',
+  marginSmall:  '10px',
+  marginLarge:  '30px',
+  paddingBase:  '15px',
+  paddingSmall: '10px',
+  paddingLarge: '30px',
+}
+
+export default metrics
